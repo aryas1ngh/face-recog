88a4681c761cd66ebc71d3f3d0199c19e93700f4
aryas1ngh <97499206+aryas1ngh@users.noreply.github.com>
2024-01-02 18:10:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,8 +15,7 @@ recognizer.read(trainer_dir+'lbph_trainer.yml')
 face_cascade = cv2.CascadeClassifier(cascade_dir)
 font = cv2.FONT_HERSHEY_SIMPLEX
 id = 0
-names = ['arya', 'aditya', 'messi'] 
-
+names = ['arya', 'aditya', 'messi'] # put according to ID
 
 cam = cv2.VideoCapture(0)
 cam.set(3, 640) #w
@@ -26,7 +25,6 @@ minW = 0.1*cam.get(3)
 minH = 0.1*cam.get(4)
 while True:
     ret, img =cam.read()
-    # img = cv2.flip(img, -1)
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     
     faces = face_cascade.detectMultiScale( 
@@ -35,6 +33,7 @@ while True:
         minNeighbors = 5,
         minSize = (int(minW), int(minH)),
        )
+    
     for(x,y,w,h) in faces:
         cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
         id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
@@ -46,29 +45,14 @@ while True:
             id = "unknown"
             confidence = "  {0}%".format(round(100 - confidence))
         
-        cv2.putText(
-                    img, 
-                    str(id), 
-                    (x+5,y-5), 
-                    font, 
-                    1, 
-                    (255,255,255), 
-                    2
-                   )
-        cv2.putText(
-                    img, 
-                    str(confidence), 
-                    (x+5,y+h-5), 
-                    font, 
-                    1, 
-                    (255,255,0), 
-                    1
-                   )  
+        cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
+        cv2.putText(img, str(confidence),  (x+5,y+h-5), font, 1, (255,255,0),  1)  
+
+        # checks for new faces every 0.2 seconds
         time.sleep(0.2)
         if id not in recognized_names:
             recognized_names.append(id)
                 
-    
     cv2.imshow('camera',img) 
     k = cv2.waitKey(10) & 0xff
     if k == 27:
